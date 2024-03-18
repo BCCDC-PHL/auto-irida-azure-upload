17d16e4298fdaf81f13318cec8d4e8ea978f3443
Dan Fornika <dfornika@gmail.com>
2024-03-18 15:17:06 -0700

    Fix samplesheet test

--- tests/test_samplesheet.py
+++ tests/test_samplesheet.py
@@ -23,11 +23,12 @@ class TestFindSampleSheets(unittest.TestCase):
         }
 
         sequencing_run = {
-            'path': os.path.join(TEST_DATA_DIR, 'run_parent_dirs/M00123/22/220101_M00123_0001_000000000-AAA11/SampleSheet.csv'),
+            'path': os.path.join(TEST_DATA_DIR, 'run_parent_dirs/M00123/22/220101_M00123_0001_000000000-AAA11'),
             'sequencing_run_id': '220101_M00123_0001_000000000-AAA11',
             'instrument_type': 'miseq',
         }
         expected_samplesheet_paths = [
+            os.path.join(TEST_DATA_DIR, 'run_parent_dirs/M00123/22/220101_M00123_0001_000000000-AAA11/SampleSheet.csv'),
         ]
         actual_samplesheet_paths = samplesheet.find_samplesheets(sequencing_run['path'], sequencing_run['instrument_type'])
 
